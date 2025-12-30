b499543165ef403baad850c3abd25041c8cae2b5
papumaja <papumaja@protonmail.com>
2025-12-30 15:02:14 +0200

    Add explanation for missing features

--- src/commands/start.py
+++ src/commands/start.py
@@ -1,5 +1,13 @@
 from telegram import Update
 from telegram.ext import CallbackContext
 
+
+GITHUB_URL = "https://github.com/Papumaja/talonmies_bot"
+
+
 async def cmd_start(update: Update, context):
     await context.bot.send_message(chat_id=update.effective_chat.id, text="Morjensta pöytään!")
+
+
+async def cmd_feature_request(update: Update, context):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text=f"Koodaa ite ja tee PR: {GITHUB_URL}")
